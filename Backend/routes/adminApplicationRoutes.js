a5874ab0d327c9a925933468d0f566565f58369d
Ankita Das <daszankita23@gmail.com>
2026-08-29 12:57:38 +0530

    updateApplicationStatus route added

--- Backend/routes/adminApplicationRoutes.js
+++ Backend/routes/adminApplicationRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getAdminApplicationById, getAllApplications } from "../controllers/applicationController.js";
+import { getAdminApplicationById, getAllApplications, updateApplicationStatus } from "../controllers/applicationController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 router.get("/", authMiddleware, adminMiddleware, getAllApplications);
 router.get("/:id", authMiddleware, adminMiddleware,getAdminApplicationById);
+router.patch("/:id", authMiddleware, adminMiddleware,updateApplicationStatus);
+
+
 
 export default router;
