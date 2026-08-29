7d21a5a730e1a3600b63da9aa12f2cf105a6141d
Ankita Das <daszankita23@gmail.com>
2026-08-29 16:15:20 +0530

    application statistics route added

--- Backend/routes/adminDashboardRoutes.js
+++ Backend/routes/adminDashboardRoutes.js
@@ -2,8 +2,9 @@ import express from "express";
 
 import {
   getDashboardSummary,
+  getJobStats,
+  getApplicationStats
 } from "../controllers/dashboardController.js";
-
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
@@ -15,5 +16,19 @@ router.get(
   adminMiddleware,
   getDashboardSummary
 );
+router.get(
+  "/job-stats",
+  authMiddleware,
+  adminMiddleware,
+  getJobStats
+);
+router.get(
+  "/application-stats",
+  authMiddleware,
+  adminMiddleware,
+  getApplicationStats
+);
+
+
 
 export default router;
